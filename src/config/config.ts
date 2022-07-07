da15f75c84585a6ae290a0c2d8c9cf5503760e12
lehenshtein <lehenshtein@gmail.com>
2022-07-07 23:09:16 +0300

    heroku changes

--- src/config/config.ts
+++ src/config/config.ts
@@ -6,7 +6,7 @@ const MONGO_USERNAME = process.env.MONGO_USERNAME || '';
 const MONGO_PASSWORD = process.env.MONGO_PASSWORD || '';
 const MONGO_URL = `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@cluster0.i2yk4qd.mongodb.net/`;
 
-const PORT = process.env.PORT  ? Number(process.env.PORT ) : 3000;
+const PORT = process.env.PORT ? Number(process.env.PORT) : 3000;
 
 export const config = {
   mongo: {
