66217c4f21644c0c5471a773a2930162e783d70a
lehenshtein <lehenshtein@gmail.com>
2022-07-13 21:37:37 +0300

    route update

--- src/routes/Posts.router.ts
+++ src/routes/Posts.router.ts
@@ -7,7 +7,7 @@ const router = express.Router();
 router.post('', ValidateSchema(Schema.post.create), controller.createPost);
 router.get('/:postId', controller.readPost);
 router.get('', controller.readAll);
-router.patch('/patch/:postId', ValidateSchema(Schema.post.update), controller.updatePost);
+router.patch('/:postId', ValidateSchema(Schema.post.update), controller.updatePost);
 router.delete('/:postId', controller.deletePost);
 
 export = router;
