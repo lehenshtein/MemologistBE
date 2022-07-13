4e63cb326d453d85ed018cb416c5606492468693
lehenshtein <lehenshtein@gmail.com>
2022-07-13 21:16:46 +0300

    routes and readme updates

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # MemologistBE
+
+1. To run application you need to create .env file in the root folder
+2. Add to .env file:
+   MONGO_USERNAME = "<username>"
+   MONGO_PASSWORD = "<password>"
+   PORT = "3000" //optional
+3. Run - npm run watch in the terminal
--- src/routes/Posts.router.ts
+++ src/routes/Posts.router.ts
@@ -4,10 +4,10 @@ import { Schema, ValidateSchema } from '../middleware/ValidateSchema';
 
 const router = express.Router();
 
-router.post('/create', ValidateSchema(Schema.post.create), controller.createPost);
-router.get('/get/:postId', controller.readPost);
-router.get('/get', controller.readAll);
+router.post('', ValidateSchema(Schema.post.create), controller.createPost);
+router.get('/:postId', controller.readPost);
+router.get('', controller.readAll);
 router.patch('/patch/:postId', ValidateSchema(Schema.post.update), controller.updatePost);
-router.delete('/delete/:postId', controller.deletePost);
+router.delete('/:postId', controller.deletePost);
 
 export = router;
