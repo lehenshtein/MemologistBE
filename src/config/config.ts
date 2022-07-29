ff0079d4b5c58a12261715550a12a741a96810a7
lehenshtein <lehenshtein@gmail.com>
2022-07-29 22:20:23 +0300

    config and db changes for prod

--- src/config/config.ts
+++ src/config/config.ts
@@ -5,10 +5,9 @@ const MONGO_USERNAME = process.env.MONGO_USERNAME || '';
 const MONGO_PASSWORD = process.env.MONGO_PASSWORD || '';
 const NODE_ENV = process.env.NODE_ENV || 'dev';
 const CLUSTER_NAME = process.env.CLUSTER_NAME || '';
-let MONGO_URL = `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@cluster0.i2yk4qd.mongodb.net/`;
-if (NODE_ENV === 'prod') {
-  MONGO_URL = `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@cluster0.${CLUSTER_NAME}.mongodb.net`;
-}
+const MONGO_URL = NODE_ENV === 'prod'
+  ? `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@cluster0.${CLUSTER_NAME}.mongodb.net`
+  : `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@cluster0.i2yk4qd.mongodb.net/`;
 
 const PORT = process.env.PORT ? Number(process.env.PORT) : 3000;
 
