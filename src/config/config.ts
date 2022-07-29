d68f9879d4a9b9a2b57c9a177bffe2ccd2b6ffff
lehenshtein <lehenshtein@gmail.com>
2022-07-29 22:55:20 +0300

    config and db changes for prod

--- src/config/config.ts
+++ src/config/config.ts
@@ -4,9 +4,9 @@ dotenv.config();
 const MONGO_USERNAME = process.env.MONGO_USERNAME || '';
 const MONGO_PASSWORD = process.env.MONGO_PASSWORD || '';
 const NODE_ENV = process.env.NODE_ENV || 'dev';
-const CLUSTER_NAME = process.env.CLUSTER_NAME || 'zgectgh';
+const CLUSTER_NAME = process.env.CLUSTER_NAME || '';
 const MONGO_URL = NODE_ENV === 'prod'
-  ? `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@cluster0.zgectgh.mongodb.net`
+  ? `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@cluster0.${CLUSTER_NAME}.mongodb.net`
   : `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@cluster0.i2yk4qd.mongodb.net/`;
 
 const PORT = process.env.PORT ? Number(process.env.PORT) : 3000;
