822dd362113dbd4d3ca0ed5b559e769d8650f478
lehenshtein <lehenshtein@gmail.com>
2022-08-27 15:51:20 +0300

    fix(post fixes and self request): file error changed from 401 to 400, self request url changed to login page

--- src/server.ts
+++ src/server.ts
@@ -27,7 +27,7 @@ if (config.env === 'prod') {
 function StartServer () {
   setInterval(function () {
     http.get(selfUrl);
-    http.get(config.frontUrl);
+    http.get(config.frontUrl + 'auth/sign-in');
   }, 1200000); // every 20 minutes (1200000)
 
   if (config.env !== 'local') { // disabling cron for local env
